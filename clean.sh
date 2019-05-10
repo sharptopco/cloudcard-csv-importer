4ad839169166d6ca29ca09c1be27d6f8a797f47a
Tony Erskine <tony@sharptop.co>
2019-05-10 16:46:04 -0400

    fixed clean.sh

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-[ -e "../input/test-people.csv" ] && echo "../input/test-people.csv is already replaced" || mv ../completed/test-people.csv ../input/test-people.csv
+[ -e "../input/test-people.csv" ] && echo "../input/test-people.csv is already replaced" || mv completed/test-people.csv input/test-people.csv
 
-for f in ../output/*.csv; do
+for f in output/*.csv; do
     [ -e "$f" ] && rm "$f" || echo "$f is already clean"
     break
 done
